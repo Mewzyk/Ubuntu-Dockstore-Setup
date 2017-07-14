04093924ab1389bb18753a66f2f0b2aaeff5ea69
sgthomas <mewzyk@gmail.com>
2017-07-14 19:57:05 +0000

    Finished script to be fully automated. Needs testing

--- setup_h3agatk.sh
+++ setup_h3agatk.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+printf "\n Intial Ubunutu update \n"
+
 # Initial update
 sudo apt-get update && sudo apt-get -y upgrade
 
+printf "\n Installing pip \n"
+
 # Install pip
 sudo apt-get install python-pip
+
+printf "\n Installing cwltool \n"
 
 # Install cwltool
 pip install setuptools==28.8.0
 pip install cwl-runner cwltool==1.0.20170217172322 schema-salad==2.2.20170222151604 avro==1.8.1
 
+printf "\n Installing Java \n"
+
 # Java installation
 sudo apt-get update
 sudo apt-get install default-jre
 sudo apt-get install default-jdk
+
+printf "\n Installing Docker \n "
 
 # Install Docker
 sudo apt-get update
@@ -36,11 +46,15 @@ sudo apt-get install docker-ce
 apt-cache madison docker-ce
 sudo apt-get install docker-ce=17.06.0~ce-0~ubuntu-xenial
 
+printf "\n Installing Dockstore \n"
 
 # Dockstore setup
-wget 'https://github.com/ga4gh/dockstore/releases/download/1.2.3/dockstore'
-
 mkdir ~/.dockstore
 
-echo 'token: 8b45f789e5f0255228a4d91b5e27b5af29bdbd4f2a2c0d5c93e55630ceb87d68
-server-url: https://dockstore.org:8443' > ~/.dockstore/config
+cat docker_config.txt > ~/.dockstore/config
+
+chmod +x dockstore
+
+cat path_config.txt > ~/.bashrc
+
+dockstore
